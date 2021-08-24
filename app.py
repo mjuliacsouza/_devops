32dc12dc868a7300097890e32635319ab540ec4b
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-08-24 19:14:19 -0300

    :tada: estudos 24/08

--- app.py
+++ app.py
@@ -20,10 +20,27 @@ def home1():
 
 
 @app.route('/')
-def list():
+def list1():
     todo_list = Todo.query.all()
     return render_template('list.html', todo_list=todo_list)
 
+
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home1"))
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home1"))
 
 if __name__ == '__main__':
     app.run()
