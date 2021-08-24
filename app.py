b88c07838d39dab05501680500bff0473ec0c525
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-08-24 19:17:54 -0300

    :tada: estudos 24/08

--- app.py
+++ app.py
@@ -34,11 +34,18 @@ def add():
     return redirect(url_for("home1"))
 
 
-@app.route('/add', methods=['POST'])
-def add():
-    title = request.form.get("title")
-    new_todo = Todo(title=title, complete=False)
-    db.session.add(new_todo)
+@app.route('/update/<int:todo_id>')
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("home1"))
+
+
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("home1"))
 
